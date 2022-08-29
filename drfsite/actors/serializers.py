3e49b671bd9fa70be53495cf3eb98d2525ce6d60
Alexander <troshiy2011@mail.ru>
2022-08-29 14:17:02 +0300

    onchansed view

--- drfsite/actors/serializers.py
+++ drfsite/actors/serializers.py
@@ -1,33 +1,14 @@
-from dataclasses import fields
 import io
-from turtle import title
 from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-
 from .models import *
-
-class ActorModel:
-    def __init__(self, title, content) -> None:
-        self.title = title
-        self.content = content
 
 
 class ActorSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)#имена переменных сериализатора должны совпадать с именами сериулизуемой модели
     content = serializers.CharField()# max_length не обязателен в сериализаторе
-
-
-def encode():
-    model = ActorModel('Творог','Девятипроцентный творог коровка из кореновки')
-    model_serialized = ActorSerializer(model)
-    print(model_serialized.data,type(model_serialized.data),sep='\n')
-    json = JSONRenderer().render(model_serialized.data)
-    print(json)
-
-def decode():
-    stream = io.BytesIO(b'{"title": "Tvorog", "content": "Nine percent tvorog korovka iz korenovki"}') 
-    data = JSONParser().parse(stream)
-    serializer = ActorSerializer(data=data)
-    serializer.is_valid()
-    print(serializer.validated_data)
+    time_create = serializers.DateTimeField(read_only=True)#read_only=True нужен для указания необязательных полей при POST запросе(они создадутся автоматически)
+    time_update = serializers.DateTimeField(read_only=True)
+    cat_id = serializers.IntegerField()
+    slug = serializers.CharField(max_length=255,read_only=True)
--- drfsite/actors/views.py
+++ drfsite/actors/views.py
@@ -1,6 +1,3 @@
-from django.forms import model_to_dict
-from django.shortcuts import render
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -10,13 +7,16 @@ from .models import *
 
 class ActorAPIView(APIView):
     def get(self, request):
-        lst = Actor.objects.all().values()#queryset не serializeble нужно values
-        return Response({'actors': list(lst)}) 
+        lst = Actor.objects.all()#queryset не serializeble нужно values
+        return Response({'actors': ActorSerializer(lst,many=True).data}) #many=True нужен для бработки списка записей а не одной | .data словарь преобразованных данных
 
     def post(self, request):
+        serializer = ActorSerializer(data=request.data)#нужно указывать data=
+        serializer.is_valid(raise_exception=True)#проверка валидности данных чтобы выдавалась ошибка не джанговская желтая, а json форматированная ошибка
+
         new_post = Actor.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(new_post)})
+        return Response({'post': ActorSerializer(new_post).data})
