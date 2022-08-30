e6701c09b240ea827460b08a319fcfe1c15abe9e
Alexander <troshiy2011@mail.ru>
2022-08-30 13:34:36 +0300

    removed unused imports

--- drfsite/actors/serializers.py
+++ drfsite/actors/serializers.py
@@ -1,7 +1,4 @@
-import io
 from rest_framework import serializers
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from .models import *
 
 
--- drfsite/actors/views.py
+++ drfsite/actors/views.py
@@ -1,4 +1,3 @@
-from webbrowser import get
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
