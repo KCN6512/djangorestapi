7e3ff69e2a9353b00d1d5a0b3a7c50939b7b1949
Alexander <troshiy2011@mail.ru>
2022-09-04 14:05:51 +0300

    добавил пример во вьюшку и сделал вывод категории вместо id

--- drfsite/actors/serializers.py
+++ drfsite/actors/serializers.py
@@ -3,11 +3,11 @@ from .models import *
 
 
 class ActorSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='cat.name')
     class Meta:
         model = Actor
-        fields = '__all__'
+        fields = ['title','content','time_create','time_update','cat', 'category_name']
         read_only_fields = ('slug',)
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
--- drfsite/actors/views.py
+++ drfsite/actors/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .serializers import *
 from .models import *
 from rest_framework.decorators import action
-
+#py drfsite/manage.py runserver
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
@@ -15,6 +15,9 @@ class ActorViewSet(viewsets.ModelViewSet):
             return Actor.objects.all()
         
         return Actor.objects.filter(pk=pk)
+    
+    # def list(self, request):
+    #     return Response({'пример': 'П Р И М Е Р'})
 
     # @action(methods=['get'], detail=False)#detail определяет возможность использовать pk и url ПЕРЕД category т.е. actors/pk/category
     # def category(self, request, pk = None):
@@ -22,6 +25,6 @@ class ActorViewSet(viewsets.ModelViewSet):
     #     return Response({'categories': [i.name for i in categories]})
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):#гораздо проще создать новый вьюсет чем action
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
