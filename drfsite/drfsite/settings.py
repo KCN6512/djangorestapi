a2e4a2caa11653a9ebd96e0f2a35521448f9b6f4
Alexander <troshiy2011@mail.ru>
2022-09-05 16:34:36 +0300

    default pagination

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -131,9 +131,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated'
     ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
